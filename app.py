32992a2d9263395fae5794f01fdb56a035e13006
ssamad07 <122140316+ssamad07@users.noreply.github.com>
2023-01-07 14:13:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ n=col1.number_input('Enter Nitrogen')
 p=col2.number_input('Enter Phosphorous')
 k=col1.number_input('Enter Potassium')
 t=col2.number_input('Enter Temperature')
-h=col1.nummber_input('Enter Humidity')
+h=col1.number_input('Enter Humidity')
 ph=col2.number_input('Enter pH')
 r=col1.number_input('Enter Rainfall')
 
 if st.button('Predict'):
     data=[[n,p,k,t,h,ph,r]]
-    model.predict(data) [0]
+    result = model.predict(data) [0]
     st.success(result)
